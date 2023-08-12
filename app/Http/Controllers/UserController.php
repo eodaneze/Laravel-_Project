527d2b5ef0441ca29fb54ff96ef5f211b7773f1b
eodaneze <ezealiokechukwu1999@gmail.com>
2023-08-11 20:58:55 -0700

    inserting data to database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,9 +11,11 @@ class UserController extends Controller
         $incomingFields = $request->validate([
             'name'=> ['required', 'min:3', 'max:10'],
             'email'=>['required', 'email'],
-            'password'=>['required', 'min:8', 'max:30'],
+            'password'=>['required', 'min:8', 'max:30']
         ]);
-        
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
+
         return "Hello from our controller";
     }
 }
